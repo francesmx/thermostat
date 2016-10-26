74140ee252ac1a27a4cac7650f4ff05263d87663
andy jenkins <andygjenkins@gmail.com>
2016-10-26 11:14:09 +0100

    Add test for min temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,20 +8,27 @@ describe('Thermostat', function(){
     thermostat = new Thermostat();
   });
 
-  it('starts with temperature at 20', function(){
-    expect(thermostat.temperature).toBe(20)
+  describe('start conditions', function(){
+
+    it('starts with temperature at 20', function(){
+      expect(thermostat.temperature).toEqual(20)
+    });
+
+    it('has a minimum temperature', function(){
+      expect(thermostat.MIN_TEMPERATURE).toEqual(10)
+    });
   });
 
   describe('changing temperature', function(){
 
     it('increasing the temperature', function(){
       thermostat.increaseTemp(2);
-      expect(thermostat.temperature).toBe(22)
+      expect(thermostat.temperature).toEqual(22)
     });
 
     it('decreasing the temperature', function(){
       thermostat.decreaseTemp(2);
-      expect(thermostat.temperature).toBe(18)
+      expect(thermostat.temperature).toEqual(18)
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,13 @@
+'use strict';
+
 function Thermostat(){
   this.temperature = 20
 }
 
 Thermostat.prototype.increaseTemp = function(amount){
   this.temperature += amount;
+}
+
+Thermostat.prototype.decreaseTemp = function (amount) {
+  this.temperature -= amount;
 }
