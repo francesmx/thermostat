4597d0a89b85fccde0e95a91ca6836c83f5a7d6b
Frances Maxwell <fmaxwell@gmail.com>
2016-10-26 11:05:39 +0100

    Passed test for increasing temp and wrote failing test for decreasing temp.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,15 +8,20 @@ describe('Thermostat', function(){
     thermostat = new Thermostat();
   });
 
-
   it('starts with temperature at 20', function(){
     expect(thermostat.temperature).toBe(20)
   });
 
   describe('changing temperature', function(){
+
     it('increasing the temperature', function(){
       thermostat.increaseTemp(2);
       expect(thermostat.temperature).toBe(22)
+    });
+
+    it('decreasing the temperature', function(){
+      thermostat.decreaseTemp(2);
+      expect(thermostat.temperature).toBe(18)
     });
   });
 });
